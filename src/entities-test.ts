85ac9db40ec245badc63a2e0e02f9bc84f09af07
Horia Coman <horia141@gmail.com>
2018-02-11 10:35:45 +0200

    Add Picture, PictureSet and friends

--- src/entities-test.ts
+++ src/entities-test.ts
@@ -1,18 +1,27 @@
 import { expect } from 'chai'
 import 'mocha'
 
-import { Event, SubEventDetails } from './entities'
+import { Event, Picture, PictureSet, SubEventDetails } from './entities'
 
 
 describe('Event', () => {
     describe('doesLookActive', () => {
+        const picture = new Picture();
+        picture.position = 0;
+        picture.uri = 'https://example.com/picture.jpeg';
+        picture.width = Picture.DEFAULT_WIDTH;
+        picture.height = Picture.DEFAULT_HEIGHT;
+
         const subEventDetails1 = new SubEventDetails();
         subEventDetails1.haveEvent = true;
+
         const subEventDetails2 = new SubEventDetails();
         subEventDetails2.haveEvent = false;
 
-        it('should return true for an event with active subevents', () => {
+        it('should return true for an event with active subevents and pictures', () => {
             const event = new Event();
+            event.pictureSet = new PictureSet();
+            event.pictureSet.pictures = [picture];
             event.subEventDetails = [
                 subEventDetails1,
                 subEventDetails2
@@ -21,8 +30,22 @@ describe('Event', () => {
             expect(event.doesLookActive).to.be.true;
         });
 
+        it('should return false for an event with no pictures', () => {
+            const event = new Event();
+            event.pictureSet = new PictureSet();
+            event.pictureSet.pictures = [];
+            event.subEventDetails = [
+                subEventDetails1,
+                subEventDetails2
+            ];
+
+            expect(event.doesLookActive).to.be.false;
+        });
+
         it('should return false for an event with no subevents', () => {
             const event = new Event();
+            event.pictureSet = new PictureSet();
+            event.pictureSet.pictures = [picture];
             event.subEventDetails = [];
 
             expect(event.doesLookActive).to.be.false;
@@ -30,6 +53,8 @@ describe('Event', () => {
 
         it('should return false for an event with all subevents disabled', () => {
             const event = new Event();
+            event.pictureSet = new PictureSet();
+            event.pictureSet.pictures = [picture];
             event.subEventDetails = [subEventDetails2];
 
             expect(event.doesLookActive).to.be.false;
--- src/entities.ts
+++ src/entities.ts
@@ -1,7 +1,14 @@
 /** The core entities of the content service. */
 
 /** Imports. Also so typedoc works correctly. */
-import { ArrayOf, ExtractError, MarshalEnum, MarshalFrom, MarshalWith } from 'raynor'
+import {
+    ArrayOf,
+    ExtractError,
+    Marshaller,
+    MarshalEnum,
+    MarshalFrom,
+    MarshalWith
+} from 'raynor'
 import * as r from 'raynor'
 
 
@@ -16,6 +23,74 @@ export class AddressMarshaller extends r.StringMarshaller {
         }
 
         return s;
+    }
+}
+
+
+/** An uploaded picture from a user. */
+export class Picture {
+    /** The default width of an image in px. Expect all images to be of this size. */
+    public static readonly DEFAULT_WIDTH: number = 1600;
+    /** The default height of an image in px. Expect all images to be of this size. */
+    public static readonly DEFAULT_HEIGHT: number = 900;
+
+    /** The position of the picture in a {@link PictureSet}. */
+    @MarshalWith(r.PositiveIntegerMarshaller)
+    position: number;
+
+    /**
+     * The URI where the image can be viewed. It has to be secure one, but not necessarily from
+     * truesparrow
+     */
+    @MarshalWith(r.SecureWebUriMarshaller)
+    uri: string;
+
+    /** The width of the image. Currently defaults to {@link Picture.DEFAULT_WIDTH}. */
+    @MarshalWith(r.PositiveIntegerMarshaller)
+    width: number;
+
+    /** The height of the image. Currently defaults to {@link Picture.DEFAULT_HEIGHT}. */
+    @MarshalWith(r.PositiveIntegerMarshaller)
+    height: number;
+}
+
+
+/** The set of images for a user. */
+export class PictureSet {
+    /** The current maximum number of images for a user. */
+    public static readonly MAX_NUMBER_OF_PICTURES = 25;
+
+    /** The set of pictures. */
+    @MarshalWith(ArrayOf(MarshalFrom(Picture)))
+    pictures: Picture[];
+}
+
+
+/**
+ * A marshaller of {@link PictureSet}s. Ensures no more than
+ * {@link PictureSet.MAX_NUMBER_OF_PICTURES} are present.
+ */
+export class PictureSetMarshaller implements Marshaller<PictureSet> {
+    private static readonly _basicMarshaller = new (MarshalFrom(PictureSet))();
+
+    extract(raw: any): PictureSet {
+        const pictureSet = PictureSetMarshaller._basicMarshaller.extract(raw);
+
+        if (pictureSet.pictures.length > PictureSet.MAX_NUMBER_OF_PICTURES) {
+            throw new ExtractError('Expected less than MAX_NUMBER_OF_PICTURES');
+        }
+
+        for (let i = 0; i < pictureSet.pictures.length; i++) {
+            if (pictureSet.pictures[i].position != i + 1) {
+                throw new ExtractError(`Expected picture {i} position to follow the pattern`);
+            }
+        }
+
+        return pictureSet;
+    }
+
+    pack(pictureSet: PictureSet): any {
+        return PictureSetMarshaller._basicMarshaller.pack(pictureSet);
     }
 }
 
@@ -64,6 +139,10 @@ export class Event {
     @MarshalWith(MarshalEnum(EventState))
     state: EventState;
 
+    /** The set of pictures for the event. */
+    @MarshalWith(PictureSetMarshaller)
+    pictureSet: PictureSet;
+
     /** The various sub-events making up the event. */
     @MarshalWith(ArrayOf(MarshalFrom(SubEventDetails)))
     subEventDetails: SubEventDetails[]
@@ -78,10 +157,15 @@ export class Event {
 
     /**
      * Checks whether the event looks active or not.
-     * @note For now, this looks to see if there's any subevent which is allowed.
+     * @note For now, this looks to see if there's any subevent which is allowed and if there are
+     *     some pictures.
      * @return Whether the event looks active or not.
      */
     get doesLookActive(): boolean {
+        if (this.pictureSet.pictures.length == 0) {
+            return false;
+        }
+
         if (this.subEventDetails.every(sed => !sed.haveEvent)) {
             return false;
         }
